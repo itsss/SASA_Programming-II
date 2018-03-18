3f5e53cb9eaa410ed86a600ab275bad9ed3fe0a7
Taewon Kang <itschool@itsc.kr>
2018-03-18 22:44:29 +0900

    1등한 학생의 성적 Code 설명 추가

--- ch03/1등한 학생의 성적.cpp
+++ ch03/1등한 학생의 성적.cpp
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-    char name[10];
-    int s1;
-    int s2;
-    int s3;
+typedef struct
+{
+    char name[30]; //학생의 이름
+    int s1, s2, s3; //1~3번째 과목
 }student;
 
 int main()
 {
-    int n;
-    int max = -999, loc = 0, rank2 = 1, rank3 = 1;
-    scanf("%d", &n);
-    student *p = (student *)malloc(sizeof(student)*n);
-    //student p[n];
-    for(int i = 0; i < n; ++i)
+    int n, max=-999, loc=0, rank2=1, rank3=1;
+    scanf("%d", &n); //n을 입력받는다.
+    student *st = (student *)malloc(sizeof(student)*n); //구조체 동적할당
+    for(int i = 0; i < n; i++)
     {
-        scanf("%s %d %d %d", p[i].name, &p[i].s1, &p[i].s2, &p[i].s3);
-        if(p[i].s1 > max)
+        //학생의 정보 입력
+        scanf("%s %d %d %d", st[i].name, &st[i].s1, &st[i].s2, &st[i].s3);
+        if(st[i].s1 > max) //첫 번째 과목을 1등한 학생의 위치를 구한다.
         {
-            max = p[i].s1; //1등만 골라냄
-            loc = i;
+            max = st[i].s1; //1등한 학생 구별용으로 사용.
+            loc = i; //1등한 학생의 위치
         }
     }
-    for(int i = 0; i < n; ++i) //1등의 위치를 기반으로, 나머지 과목의 성적을 구해냄
-    {
-        if(p[i].s2 > p[loc].s2) rank2++;
-        if(p[i].s3 > p[loc].s3) rank3++;
+    for(int i = 0; i < n; i++)
+    { //1등한 학생의 두번째, 세번째 과목의 석차도 알아보자.
+        if(st[i].s2 > st[loc].s2) rank2++;
+        if(st[i].s3 > st[loc].s3) rank3++;
     }
-    printf("%s %d %d", p[loc].name, rank2, rank3);
-    free(p);
-    return 0;
+    //첫 번째 과목을 1등한 학생의 이름과, 두번째, 세번째 과목의 석차를 구분하여 출력.
+    printf("%s %d %d", st[loc].name, rank2, rank3);
 }
