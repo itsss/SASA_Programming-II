700f48801b24e64a27d58ac023f0ce41175727cd
Taewon Kang <itschool@itsc.kr>
2018-05-31 15:34:39 +0900

    (18.05.31) 트리 연습문제 (트리 전위, 중위, 후위 순회)

---
+++ ch10/트리의 중위 순회(배열).cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+char str[20009];
+
+void f(int x)
+{
+    if(str[2*x+1]) f(2*x+1);
+    printf("%c", str[x]);
+    if(str[2*x+2]) f(2*x+2);
+}
+
+int main()
+{
+    scanf("%s", str);
+    f(0);
+}
